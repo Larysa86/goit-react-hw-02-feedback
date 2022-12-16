810f177ee8ee46722561f05a983f357eda8b0f2d
Larysa Holtseva <holtseva@seznam.cz>
2022-12-16 13:46:49 +0100

    add changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { FeedbackOptions, Statistics, Section, Notification } from './index';
+import React, { Component } from 'react';
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = key => {
+    this.setState(prevState => ({ [key]: prevState[key] + 1 }));
+  };
+
+  totalFeedback = (a, b, c) => a + b + c;
+
+  positiveFeedbackPercentage = (positiveRating, totalRating) =>
+    totalRating ? Math.ceil((positiveRating / totalRating) * 100) : 0;
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.totalFeedback(good, neutral, bad);
+    const positivePercentage = this.positiveFeedbackPercentage(
+      good,
+      total
+    );
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {total ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,9 @@
+// import { css } from '@emotion/react';
+import PropTypes from 'prop-types';
+import css from './Notification.module.css';
+
+export const Notification = ({ message }) => <p className={css.notification}>{message}</p>;
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
